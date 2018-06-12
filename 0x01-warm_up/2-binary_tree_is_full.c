7d727d10b830d7005f52dc9756f9d14ed72bb82f
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-06-11 20:31:58 -0700

    Made a function that checks if a binary tree is full

--- 0x01-warm_up/2-binary_tree_is_full.c
+++ 0x01-warm_up/2-binary_tree_is_full.c
@@ -8,16 +8,22 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree != NULL)
-    {
-        if (tree->left == NULL && tree->right == NULL)
-        {
-            return(1);
-        }
-        else if (tree->left != NULL && tree->right != NULL)
-        {
-            return(1);
-        }
-    }
-    return(0);
+    int left, right;
+
+    if (tree == NULL)
+        return (0);
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+    if (tree->left != NULL && tree->right == NULL)
+        return (0);
+    if (tree->left == NULL && tree->right != NULL)
+        return (0);
+    
+    left = binary_tree_is_full(tree->left);
+    right = binary_tree_is_full(tree->right);
+    
+    if (left == 0 || right == 0)
+        return (0);
+    else
+        return (1);
 }
