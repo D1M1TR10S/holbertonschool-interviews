1ef6bd2385da42357573aa0c1739d53b31039019
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-06-11 17:30:56 -0700

    Write to a json file

--- 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
+++ 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
@@ -2,21 +2,21 @@
 '''
 Calls the JSON placeholder REST api
 and saves information about a given
-employee's TODO list progress in a
-json file
+employee's TODO list progress into a
+given json file
 '''
 if __name__ == "__main__":
+    import json
     import requests
-    from sys import argv
 
     new_dict = {}
     staff = requests.get(
-    'https://jsonplaceholder.typicode.com/users/').json()
+        'https://jsonplaceholder.typicode.com/users/').json()
     for item in staff:
         usr = str(item["id"])
         todo_list = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(usr)).json()
+            "https://jsonplaceholder.typicode.com/todos?userId={}"
+            .format(usr)).json()
         items = []
         for todo in todo_list:
             dct = {
@@ -26,4 +26,5 @@ if __name__ == "__main__":
                 }
             items.append(dct)
         new_dict[usr] = items
-    print(new_dict)
+    with open('todo_all_employees.json', 'w') as fp:
+        json.dump(new_dict, fp)
