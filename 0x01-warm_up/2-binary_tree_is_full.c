d191220792552c683b13755ad1afacb2900c3fcf
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-06-11 18:07:57 -0700

    Made a function that checks if a binary tree is full

---
+++ 0x01-warm_up/2-binary_tree_is_full.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: parent of node to create
+ * @value: value to insert in the binary tree
+ * Return: pointer to new node or NULL on failure
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+    if (tree != NULL)
+    {
+        if ((tree->left == NULL && tree->right == NULL) ||
+        (tree->left != NULL && tree->right != NULL))
+        {
+            return(1);
+        }
+    }
+    return(0);
+}
--- 0x01-warm_up/search_algos.h
+++ 0x01-warm_up/search_algos.h
@@ -2,5 +2,6 @@
 #define ALGOS
 
 int binary_search(int *array, size_t size, int value);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif
