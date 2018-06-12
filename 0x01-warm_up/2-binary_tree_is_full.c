c41d10bccab1d0fe7c038e67864459e4014ff0a7
Dimitriopolous <dimitriosphilliou@gmail.com>
2018-06-11 19:11:59 -0700

    Broke if statement into 2

--- 0x01-warm_up/2-binary_tree_is_full.c
+++ 0x01-warm_up/2-binary_tree_is_full.c
@@ -10,8 +10,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
     if (tree != NULL)
     {
-        if ((tree->left == NULL && tree->right == NULL) ||
-        (tree->left != NULL && tree->right != NULL))
+        if ((tree->left == NULL && tree->right == NULL)
+        {
+            return(1);
+        }
+        else if (tree->left != NULL && tree->right != NULL))
         {
             return(1);
         }
